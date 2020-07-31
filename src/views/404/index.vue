239eac6d9eda6d2bdf5cfaae114420c72cdef26e
infinity <infinitycheng@thelian.com>
2020-07-31 14:56:21 +0800

    modify 404

--- src/views/404/index.vue
+++ src/views/404/index.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    这是404页面
+    <a-result status="404"
+              title="404"
+              sub-title="对不起，您访问的页面不存在！">
+      <template #extra>
+        <!-- <a-button type="primary">
+          Back Home
+        </a-button> -->
+      </template>
+    </a-result>
   </div>
 </template>
 <script>
